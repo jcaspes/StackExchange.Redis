fd9b7cc63e75f67e5819fa76b4b91aff741cd508
Jean-Christophe Cura <jcaspes@gmail.com>
2025-09-29 11:18:54 +0200

    call new helper

--- tests/StackExchange.Redis.Tests/ParseTests.cs
+++ tests/StackExchange.Redis.Tests/ParseTests.cs
@@ -71,7 +71,7 @@ public class ParseTests(ITestOutputHelper output) : TestBase(output)
         var reader = new BufferReader(buffer);
         RawResult result;
         int found = 0;
-        while (!(result = IPhysicalConnection.TryParseResult(false, arena, buffer, ref reader, false, null, false)).IsNull)
+        while (!(result = PhysicalConnectionHelpers.TryParseResult(false, arena, buffer, ref reader, false, null, false)).IsNull)
         {
             Log($"{result} - {result.GetString()}");
             found++;
